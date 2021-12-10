31fcd62db220e4f5e564c7eb0f6d72f6d1c5e9fc
Эмран <64014322+prostoEM@users.noreply.github.com>
2021-12-10 18:22:17 +0300

    test 4

--- src/App.js
+++ src/App.js
@@ -1,110 +1,85 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 
 import axios from 'axios';
 
-//  дан JSON: "https://www.ag-grid.com/example-assets/olympic-winners.json"
-// 1. Вывести с 10 по 20 элементы массива, используя следующую структуру:
+const API = 'https://www.ag-grid.com/example-assets/olympic-winners.json'
 
-// + John Smith
-// при нажатии на "+"
-// - John Smith
-//      age: "33"
-//      sport: "swimming"
-//      country: "Germany"
-//      totals: "12"
-
-// 2. Из полученных данных с API вывести список всех стран с кол-вом медалей спортсменов, принадлежащих данной стране
-
-// Не запрещается использовать Google для уточнения работы некоторых методов, которые Вы собираетесь использовать.
-
-let App = () => {
-
-
-  const API = 'https://www.ag-grid.com/example-assets/olympic-winners.json'
-  const [arr, setArr] = useState([])
-
+const App = () => {
+  const [athleteList, setAthleteList] = useState([])
 
   useEffect(() => {
     axios.get(API)
       .then(response => {
-        setArr(response.data)
+        setAthleteList(response.data || [])
       })
-  }
-    , [])
+  }, [])
 
-  const sortListOfCountry = arr.reduce((acc, element) => {
-    let indexElement = acc.map((accEl) => accEl.country).indexOf(element.country);
-    indexElement !== -1 ?
-      (acc[indexElement].total += element.total) :
-      acc.push({ country: element.country, total: element.total });
-    return acc;
-  }, []);
+  const countrysObj = useMemo(() => {
+    return athleteList.reduce((acc, element) => {
+      const prevValue = acc[element.country] || { total: 0, country: element.country }
 
-
-
-
+      return {
+        ...acc,
+        [element.country]: {
+          ...prevValue,
+          total: prevValue.total + element.total
+        }
+      }
+    }, {})
+  }, [athleteList])
+  
 
   return (
     <div className="App">
-
-      {arr.slice(9, 19).map((u) => (
+      {athleteList.slice(9, 19).map((athlete) => (
         <User
-          key={u.athlete + u.age + u.sport + u.total}
-          name={u.athlete}
-          age={u.age}
-          sport={u.sport}
-          country={u.country}
-          totals={u.total}
+          key={athlete.athlete + athlete.age + athlete.sport + athlete.total}
+          {...athlete}
         />
       ))}
 
-      {sortListOfCountry.map(e => (
+      {Object.values(countrysObj).map(country => (
         <City
-          key={e.country + e.total}
-          country={e.country}
-          total={e.total}
+          key={country.country}
+          {...country}
         />
       ))}
-
     </div>
   );
-
 }
 
-let User = ({ name, age, sport, country, totals }) => {
+const User = ({ athlete, age, sport, country, total }) => {
   const [isOpen, setIsOpen] = useState(false)
+
   const onButtonClick = () => {
     setIsOpen(!isOpen);
   }
+
   return (
     <div >
       <p>
         <button onClick={onButtonClick}>{isOpen ? '-' : '+'}</button>
-        {name}
-        {isOpen ? <span>
-          <p>age:{age}</p>
-          <p>sport:{sport}</p>
-          <p>country:{country}</p>
-          <p>medals:{totals}</p>
-        </span> : ''}
+        {athlete}
+        {
+          isOpen ? <span>
+            <p>age:{age}</p>
+            <p>sport:{sport}</p>
+            <p>country:{country}</p>
+            <p>medals:{total}</p>
+          </span> : null
+        }
       </p>
 
     </div>
   )
 }
 
-
-let City = ({ country, total }) => {
+const City = ({ country, total }) => {
   return (
     <div>
-      <p>{country}:  {total}</p>
+      <p>{country}: {total}</p>
     </div>
   )
 }
 
-
 export default App;
-
-
-
